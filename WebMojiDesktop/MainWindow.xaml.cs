f86757d1e9bcd0989731f8bbec28e9fa334a6285
thehoshi <thehoshiik@gmail.com>
2026-05-18 01:19:20 +0400

    added image user control

--- WebMojiDesktop/MainWindow.xaml.cs
+++ WebMojiDesktop/MainWindow.xaml.cs
@@ -12,26 +12,7 @@ namespace WebMojiDesktop
 
         public MainWindow()
         {
-            InitializeComponent();
-
-            imageMap.Set(GestureType.Fist, "C:\\Users\\Admin\\source\\repos\\WebMoji\\WebMojiDesktop\\Image\\FistImage.jpg");
-            imageMap.Set(GestureType.OneFinger, "C:\\Users\\Admin\\source\\repos\\WebMoji\\WebMojiDesktop\\Image\\OneFingerImage.jpg");
-            imageMap.Set(GestureType.Swag, "C:\\Users\\Admin\\source\\repos\\WebMoji\\WebMojiDesktop\\Image\\SwagImage.jpg");
-
-            CameraView.GestureDetected += OnGestureDetected;
-        }
-
-        private void OnGestureDetected(GestureType gesture)
-        {
-            var path = imageMap.Get(gesture);
-            if (path == null) return;
-
-            GestureImage.Source = new BitmapImage(new Uri(path, UriKind.Relative));
-        }
-
-        private void OnClosing(object? sender, CancelEventArgs e)
-        {
-            CameraView.Stop();
+            InitializeComponent(); 
         }
     }
 }
---
+++ WebMojiDesktop/UserControl/ImageView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using WebMojiCore;
+using WebMojiDesktop.UserControl;
+
+
+namespace WebMojiDesktop.UserControl
+{
+    /// <summary>
+    /// Interaction logic for ImageView.xaml
+    /// </summary>
+    public partial class ImageView : System.Windows.Controls.UserControl
+    {
+        private readonly CameraView cameraViewInstance;
+
+        public ImageView()
+        {
+            InitializeComponent();
+
+            imageMap.Set(GestureType.Fist, "C:\\Users\\Admin\\source\\repos\\WebMoji\\WebMojiDesktop\\Image\\FistImage.jpg");
+            imageMap.Set(GestureType.OneFinger, "C:\\Users\\Admin\\source\\repos\\WebMoji\\WebMojiDesktop\\Image\\OneFingerImage.jpg");
+            imageMap.Set(GestureType.Swag, "C:\\Users\\Admin\\source\\repos\\WebMoji\\WebMojiDesktop\\Image\\SwagImage.jpg");
+
+            cameraViewInstance = new CameraView();
+            cameraViewInstance.GestureDetected += OnGestureDetected;
+        }
+
+        private void OnGestureDetected(GestureType gesture)
+        {
+            var path = imageMap.Get(gesture);
+            if (path == null) return;
+
+            GestureImage.Source = new BitmapImage(new Uri(path, UriKind.Relative));
+        }
+
+        private void OnClosing(object? sender, CancelEventArgs e)
+        {
+            cameraViewInstance.Stop();
+        }
+
+    }
+}
